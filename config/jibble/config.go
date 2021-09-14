3e800dd03b534ee3512204b068a4ed939550eb05
markkizz <mark.pattanapara@gmail.com>
2021-09-15 00:48:59 +0700

    feat: Add jibble timetracker config url

--- config/jibble/config.go
+++ config/jibble/config.go
@@ -3,16 +3,17 @@ package jibble
 import "os"
 
 type JibbleConfig struct {
-	Username, Password, ApiIdentityUrl string
+	Username, Password, ApiIdentityUrl, ApiTimeTrackingUrl string
 }
 
 const (
-	USERNAME         string = "JIBBLE_USERNAME"
-	PASSWORD         string = "JIBBLE_PASSWORD"
-	API_IDENTITY_URL string = "JIBBLE_API_IDENTITY_URL"
+	USERNAME             string = "JIBBLE_USERNAME"
+	PASSWORD             string = "JIBBLE_PASSWORD"
+	API_IDENTITY_URL     string = "JIBBLE_API_IDENTITY_URL"
+	API_TIME_TRACKER_URL string = "JIBBLE_API_TIME_TRACKER_URL"
 )
 
 func LoadJibbleConfig() JibbleConfig {
-	jibbleConfig := JibbleConfig{os.Getenv(USERNAME), os.Getenv(PASSWORD), os.Getenv(API_IDENTITY_URL)}
+	jibbleConfig := JibbleConfig{os.Getenv(USERNAME), os.Getenv(PASSWORD), os.Getenv(API_IDENTITY_URL), os.Getenv(API_TIME_TRACKER_URL)}
 	return jibbleConfig
 }
